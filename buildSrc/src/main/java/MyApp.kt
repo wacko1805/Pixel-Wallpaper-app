06be26fc9a7fa547d17ef5277f26c9b68c4ae566
jack <jackccsam@gmail.com>
2021-12-22 17:55:30 +1100

    Fixed Wallpapers not showing

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.wacko1805.pixel.wallpapers"
-    const val version = 103
-    const val versionName = "1.0.3"
+    const val version = 104
+    const val versionName = "1.0.4"
 }
